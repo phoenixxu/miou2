9ca76ee4c196a408db78c246217468bcecf8ebb7
dingzhongchang <zhongchang.ding@gmail.com>
2015-03-06 14:48:26 +0800

    android studio

---
+++ miou/miou/src/main/java/com/datang/miou/views/MainActivity.java
@@ -0,0 +1,35 @@
+package com.datang.miou.views;
+
+import android.content.Intent;
+import android.view.View;
+
+import com.datang.miou.ActivitySupport;
+import com.datang.miou.R;
+import com.datang.miou.annotation.AfterView;
+import com.datang.miou.annotation.AutoView;
+import com.datang.miou.views.gen.GenActivity;
+
+/**
+ * 程序主界面
+ * 
+ * @author suntongwei
+ */
+@AutoView(R.layout.main)
+public class MainActivity extends ActivitySupport {
+
+	
+	@AfterView
+	private void init() {
+		
+		/**
+		 * 通用测试
+		 */
+		f(R.id.main_btn_gen).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(mContext, GenActivity.class));
+			}
+		});
+		
+	}
+}
