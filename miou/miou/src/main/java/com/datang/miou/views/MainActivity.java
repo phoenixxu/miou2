51beba88aa66f78a0e4e6f38332095373e075ae0
dingzhongchang <zhongchang.ding@gmail.com>
2015-03-07 17:50:58 +0800

    新增用户感知

--- miou/miou/src/main/java/com/datang/miou/views/MainActivity.java
+++ miou/miou/src/main/java/com/datang/miou/views/MainActivity.java
@@ -9,36 +9,47 @@ import com.datang.miou.annotation.AfterView;
 import com.datang.miou.annotation.AutoView;
 import com.datang.miou.views.data.DataActivity;
 import com.datang.miou.views.gen.GenActivity;
+import com.datang.miou.views.percept.PerceptionActivity;
 
 /**
  * 程序主界面
- * 
+ *
  * @author suntongwei
  */
 @AutoView(R.layout.main)
 public class MainActivity extends ActivitySupport {
 
-	
-	@AfterView
-	private void init() {
-		
-		/**
-		 * 通用测试
-		 */
-		f(R.id.main_btn_gen).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				startActivity(new Intent(mContext, GenActivity.class));
-			}
-		});
+
+    @AfterView
+    private void init() {
+
+        /**
+         * 通用测试
+         */
+        f(R.id.main_btn_gen).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startActivity(new Intent(mContext, GenActivity.class));
+            }
+        });
         /**
          * 数据管理
          */
         f(R.id.main_btn_datas).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(mContext,DataActivity.class));
+                startActivity(new Intent(mContext, DataActivity.class));
             }
         });
-	}
+
+        /**
+         * 通用测试
+         */
+        f(R.id.main_btn_cus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, PerceptionActivity.class));
+            }
+        });
+    }
 }
