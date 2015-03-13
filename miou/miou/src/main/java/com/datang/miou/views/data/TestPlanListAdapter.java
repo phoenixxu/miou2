0b5f5af8feea8722b739394ff5d9614db0c9a7ec
dingzhongchang <zhongchang.ding@gmail.com>
2015-03-13 18:02:38 +0800

    新增测试序列业务--新增和删除界面

--- miou/miou/src/main/java/com/datang/miou/views/data/TestPlanListAdapter.java
+++ miou/miou/src/main/java/com/datang/miou/views/data/TestPlanListAdapter.java
@@ -4,25 +4,50 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.datang.miou.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by dingzhongchang on 2015/3/6.
  */
-public class TestPlanListAdapter extends ArrayAdapter<TestPlanInfo> {
+public class TestPlanListAdapter extends BaseAdapter implements Filterable {
 
     private final Context mContext;
-    private final TestPlanInfo[] mItems;
+    private final List<TestPlanInfo> mItems;
     private final LayoutInflater mLayoutInflater;
 
+
     public TestPlanListAdapter(Context context, TestPlanInfo[] objects) {
-        super(context, R.layout.data_list_item, objects);
+        super();
         this.mContext = context;
-        this.mItems = objects;
+        this.mItems = new LinkedList<>(Arrays.asList(objects));
         mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
     }
 
     @Override
@@ -33,12 +58,104 @@ public class TestPlanListAdapter extends ArrayAdapter<TestPlanInfo> {
             v = mLayoutInflater.inflate(R.layout.data_list_item, parent, false);
         }
 
-        TestPlanInfo info = mItems[position];
+        if (mItems.size() > position) {
 
-        if (info != null) {
-//            ((TextView) v.findViewById(R.id.tv_creator_id)).setText(info.creatorId);
+            final TestPlanInfo info = mItems.get(position);
 
+            if (info != null) {
+                final CheckBox checkBox = (CheckBox) v.findViewById(R.id.cb_checked);
+                checkBox.setChecked(info.isChecked);
+                checkBox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        info.isChecked = checkBox.isChecked();
+                    }
+                });
+                ((TextView) v.findViewById(R.id.tv_name)).setText(info.name);
+                ((TextView) v.findViewById(R.id.tv_creator_id)).setText(info.creatorId);
+                ((TextView) v.findViewById(R.id.tv_modified_time)).setText(info.modifiedTime);
+                ((TextView) v.findViewById(R.id.tv_create_time)).setText(info.createTime);
+                ((TextView) v.findViewById(R.id.tv_exed_num)).setText(info.exedNum);
+            }
         }
+
         return v;
+    }
+
+    public void selectAll(boolean isSelected) {
+        if (mItems == null || mItems.isEmpty()) return;
+        for (TestPlanInfo info : mItems) {
+            if (info == null) continue;
+            info.isChecked = isSelected;
+        }
+        notifyDataSetChanged();
+    }
+
+    public void del() {
+        if (mItems == null || mItems.isEmpty()) return;
+        Iterator<TestPlanInfo> iterator = mItems.iterator();
+        while (iterator.hasNext()) {
+            TestPlanInfo info = iterator.next();
+            if (info.isChecked) {
+                iterator.remove();
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+
+    public TestPlanInfo getSelected() {
+        if (mItems == null || mItems.isEmpty()) return null;
+        Iterator<TestPlanInfo> iterator = mItems.iterator();
+        while (iterator.hasNext()) {
+            TestPlanInfo info = iterator.next();
+            if (info.isChecked) {
+                return info;
+            }
+        }
+        return null;
+    }
+
+    public void add(TestPlanInfo admin) {
+        if (admin != null) {
+            mItems.add(admin);
+        }
+
+        notifyDataSetChanged();
+
+    }
+
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                constraint = constraint.toString().toLowerCase();
+                FilterResults result = new FilterResults();
+
+                if (constraint != null && constraint.toString().length() > 0) {
+                    List<TestPlanInfo> founded = new ArrayList<>();
+                    for (TestPlanInfo item : mItems) {
+                        if (item.toString().toLowerCase().contains(constraint)) {
+                            founded.add(item);
+                        }
+                    }
+
+                    result.values = founded;
+                    result.count = founded.size();
+                } else {
+                    result.values = mItems;
+                    result.count = mItems.size();
+                }
+                return result;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mItems = (List<TestPlanInfo>) results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
--- miou/miou/src/main/java/com/datang/miou/views/data/TestPlanManagerFragment.java
+++ miou/miou/src/main/java/com/datang/miou/views/data/TestPlanManagerFragment.java
@@ -1,12 +1,14 @@
 package com.datang.miou.views.data;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
@@ -19,18 +21,26 @@ import com.datang.miou.R;
 public class TestPlanManagerFragment extends Fragment {
 
 
+    private static final int REQUEST_NEWPLAN = 1000;
+    public static final String RESULT_NEWPLAN = "NEW_PALN";
     private SearchView sv;
+    private TestPlanListAdapter testPlanListAdapter;
+    private FragmentActivity mContext;
+    private ListView planListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this.getActivity();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_testplan, container, false);
-        ListView listView = (ListView) root.findViewById(R.id.test_plan_listView);
-        listView.setAdapter(new TestPlanListAdapter(this.getActivity(), new TestPlanInfo[]{new TestPlanInfo()}));
+        planListView = (ListView) root.findViewById(R.id.test_plan_listView);
+        testPlanListAdapter = new TestPlanListAdapter(this.getActivity(), new TestPlanInfo[]{});
+        planListView.setAdapter(testPlanListAdapter);
+        planListView.setTextFilterEnabled(true);
 
 
 //        lv = (ListView) root.findViewById(R.id.lv);
@@ -46,15 +56,15 @@ public class TestPlanManagerFragment extends Fragment {
             //用户输入字符时激发该方法
             @Override
             public boolean onQueryTextChange(String newText) {
-//                if (TextUtils.isEmpty(newText)) {
-//                    //清楚ListView的过滤
-//                    lv.clearTextFilter();
-//
-//                } else {
-//                    //使用用户输入的内容对ListView的列表项进行过滤
-//                    lv.setFilterText(newText);
-//                }
-                return true;
+                if (TextUtils.isEmpty(newText)) {
+                    //清楚ListView的过滤
+                    planListView.clearTextFilter();
+
+                } else {
+                    //使用用户输入的内容对ListView的列表项进行过滤
+                    planListView.setFilterText(newText);
+                }
+                return false;
             }
 
             //单击搜索按钮时激发该方法
@@ -62,8 +72,8 @@ public class TestPlanManagerFragment extends Fragment {
             public boolean onQueryTextSubmit(String query) {
                 //实际应用中应该在该方法内执行实际查询
                 //此处仅使用Toast显示用户输入的查询内容
-                Toast.makeText(TestPlanManagerFragment.this.getActivity(), "您选择的是：" + query, Toast.LENGTH_SHORT).show();
-                return true;
+                Toast.makeText(mContext, "您选择的是：" + query, Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
         //设置该SearchView显示搜索按钮
@@ -71,7 +81,69 @@ public class TestPlanManagerFragment extends Fragment {
 
         //设置该SearchView内默认显示的提示文本
         sv.setQueryHint("查找");
+
+        initOnClick(root);
+
         return root;
+    }
+
+    private void initOnClick(final View root) {
+        root.findViewById(R.id.bt_plan_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 新增测试序列
+                Intent intent = new Intent(mContext, NewPlanListActivity.class);
+                TestPlanManagerFragment.this.startActivityForResult(intent, REQUEST_NEWPLAN);
+
+            }
+        });
+        root.findViewById(R.id.bt_plan_edit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 编辑测试任务
+                TestPlanInfo info = testPlanListAdapter.getSelected();
+            }
+        });
+        root.findViewById(R.id.bt_plan_download).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 测试序列下载
+            }
+        });
+        root.findViewById(R.id.bt_plan_all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 全选
+                testPlanListAdapter.selectAll(true);
+            }
+        });
+
+        root.findViewById(R.id.bt_plan_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO  删除测试序列
+                testPlanListAdapter.del();
+                scrollToBottom();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_NEWPLAN) {
+                String planName = data.getExtras().getString(RESULT_NEWPLAN);
+                //TODO 返回新建的测试序列
+                Toast.makeText(mContext, planName, Toast.LENGTH_SHORT).show();
+                testPlanListAdapter.add(new TestPlanInfo(planName, "admin"));
+                scrollToBottom();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void scrollToBottom() {
+        planListView.setSelection(testPlanListAdapter.getCount() - 1);
     }
 
     @Override
